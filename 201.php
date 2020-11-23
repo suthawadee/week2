acf4279763ae339afddbd26b37aabe5437d9427d
Taonahub <52515393+suthawadee@users.noreply.github.com>
2020-11-23 11:24:22 +0700

    Add files via upload

---
+++ 201.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>PHP Basic Syntex</title>
+    </head>
+    <body>
+        <h1>TEST: PHP</h1>
+        <?php
+           #Display echo
+           echo "<hr><h1>Hello World!</h1>";
+           echo("<h1>Header1</h1>");
+           echo("<h2>Header2</h2>");
+           echo("<h3>Header</h3>");
+           //Display Print
+           print '<h1>===Display print===</h1>';
+           print("<h1>Hello World</h1>");
+           print ("<h2>Hello World</h2>");
+        ?>
+    </body>
+</html>
---
+++ php202.php
@@ -0,0 +1,10 @@
+<?php       
+    //Php Varible
+    $message = "Hello World";
+    $message2 ="Mark";
+    $x = 5;
+    $y = 4;
+    //Display Varible
+    echo("$message.<br/>".$message2 . "<br />");
+    echo($z = $x + $y);
+?>
